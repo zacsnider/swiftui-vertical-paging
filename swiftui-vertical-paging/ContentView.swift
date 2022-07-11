38491cd68321341dd755692c57bac1a2a157996e
Zac Snider <zdsnider@gmail.com>
2022-07-11 11:50:32 -0400

    Initial Commit

---
+++ swiftui-vertical-paging/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  swiftui-vertical-paging
+//
+//  Created by Zac Snider on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-vertical-paging/swiftui_vertical_pagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_vertical_pagingApp.swift
+//  swiftui-vertical-paging
+//
+//  Created by Zac Snider on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_vertical_pagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
